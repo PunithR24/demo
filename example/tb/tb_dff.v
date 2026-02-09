1f9dd0bd21dcb37b84953d76c68a25278dba3ac4
Punith_R <163390053+PunithR24@users.noreply.github.com>
2026-02-09 10:26:20 +0530

    Add files via upload

---
+++ example/tb/tb_dff.v
@@ -0,0 +1,36 @@
+// Test Bench // test.sv
+module test (D_inter.test1 t1);
+
+    initial begin
+        t1.reset = 0;
+        t1.set   = 0;
+        t1.d     = 0;
+
+        #10
+        t1.reset = 1;
+        t1.set   = 1;
+        t1.d     = 0;
+
+        #10
+        t1.reset = 0;
+        t1.d     = 0;
+
+        #10
+        t1.d   = 1;
+        t1.set = 0;
+
+        #10
+        t1.d = 0;
+
+        #10
+        t1.d = 1;
+
+        #10
+        t1.d = 0;
+
+        #100
+        $finish;
+    end
+
+endmodule
+
