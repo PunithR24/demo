376b082998950aa10eee6bad5f1518e2a238dc76
Punith_R <163390053+PunithR24@users.noreply.github.com>
2026-02-09 10:25:23 +0530

    Add files via upload

---
+++ example/result/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1770355407 /home/student/cadWorkDir/myProjects/Digital/1BM24EC415/sim/interface/inter_dff.sv
+1770355556 /home/student/cadWorkDir/myProjects/Digital/1BM24EC415/sim/topmodule/topmod_dff.sv
+1770354010 /home/student/cadWorkDir/myProjects/Digital/1BM24EC415/sim/rtl/d_ff.v
+1770354600 /home/student/cadWorkDir/myProjects/Digital/1BM24EC415/sim/tb/tb_dff.v
